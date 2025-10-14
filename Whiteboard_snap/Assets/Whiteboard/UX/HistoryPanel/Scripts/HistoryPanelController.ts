ce93d6a89f07ada6ba4fd973095a815d42b77622
Jared Bienz <jbienz@solersoft.com>
2025-10-14 15:35:53 -0700

    Started history panel and loading data from the database.

---
+++ Whiteboard_snap/Assets/Whiteboard/UX/HistoryPanel/Scripts/HistoryPanelController.ts
@@ -0,0 +1,95 @@
+import NativeLogger from "SpectaclesInteractionKit.lspkg/Utils/NativeLogger";
+import { Database } from "../../../../DatabaseTypes";
+import { SupabaseClient, createClient } from "SupabaseClient.lspkg/supabase-snapcloud";
+import { SnapCloudRequirements } from "Examples/SnapCloudRequirements";
+
+/**
+ * Manages all aspects of the RAM including solving, visualizing, selection, etc.
+ */
+@component
+export class HistoryPanelController extends BaseScriptComponent {
+
+    /* #region [Private Variables] */
+    private client: SupabaseClient<Database>;
+    private readonly log = new NativeLogger("HistoryPanelController");
+    private uid: string;
+    /* #endregion [Private Variables] */
+
+    /* #region [Inspector Inputs] */
+    @ui.group_start('Services')
+    @input
+    @hint("Reference to SnapCloudRequirements for centralized Supabase configuration")
+    snapCloudRequirements: SnapCloudRequirements;
+    @ui.group_end
+
+    @ui.group_start('UI')
+    @input
+    @hint("Image to display snapshots")
+    image: Image;
+
+    @input
+    @hint("Text to display OCR or summary")
+    text: Text
+    @ui.group_end
+    /* #endregion [Inspector Inputs] */
+
+    /* #region [Private Methods] */
+    async initSupabase() {
+        if (!this.snapCloudRequirements || !this.snapCloudRequirements.isConfigured()) {
+            this.log.e("SnapCloudRequirements not configured");
+            return;
+        }
+
+        const supabaseProject = this.snapCloudRequirements.getSupabaseProject();
+        this.client = createClient<Database>(supabaseProject.url, supabaseProject.publicToken)
+        this.log.i("Client is ready");
+        if (this.client) {
+            await this.signInUser();
+            print("User is authenticated");
+        }
+    }
+
+    async signInUser() {
+        const { data, error } = await this.client.auth.signInWithIdToken({} as any)
+        print("Sign in user");
+        if (error) {
+            this.log.e("Sign in error: " + JSON.stringify(error));
+        } else {
+            const { user, session } = data
+            print(`User: ${JSON.stringify(user)}`);
+            print(`Session: ${JSON.stringify(session)}`);
+
+            this.uid = JSON.stringify(user.id);
+        }
+    }
+    /**
+     * Load the data for the history panel
+     * 
+     * @returns Promise<void>
+     */
+    private async loadData(): Promise<void> {
+        this.log.d("Loading Data from boardhistory table");
+
+        // Just load the first record for now
+        const data = await this.client.from("boardhistory").select("*").limit(1);
+        this.log.d("data loaded: " + JSON.stringify(data));
+    }
+    /* #endregion [Private Methods] */
+
+    /* #region [Lifecycle Methods] */
+    /**
+     * Initialize the scene manager and bind to start event
+     */
+    onAwake() {
+        this.createEvent("OnStartEvent").bind(this.onStart.bind(this));
+    }
+
+    /**
+     * Start the acoustic reflection calculation when the scene begins
+     */
+    async onStart() {
+        await this.initSupabase();
+        await this.loadData();
+    }
+    /* #endregion [Lifecycle Methods] */
+}
